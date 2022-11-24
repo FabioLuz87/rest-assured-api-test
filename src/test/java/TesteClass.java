f57656348fce0f1f02f62ce3e726351d03a667b4
FabioLuz87 <fabioddh@hotmail.com>
2022-11-24 14:10:35 -0300

    first commit

---
+++ src/test/java/TesteClass.java
@@ -0,0 +1,43 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+
+public class TesteClass {
+
+    @Test
+    public void validaApiOnline() {
+        Response responsePing =
+                given()
+                .header("Content-Type","application/json")
+                .when()
+                .get("https://treinamento-api.herokuapp.com/ping")
+        ;
+
+        responsePing
+                .then()
+                .statusCode(201)
+                .time(lessThan(2L), TimeUnit.SECONDS)
+        ;
+    }
+    @Test
+    public void ValidaListagemDeIdsDasReservas() {
+        Response responseListagemIdsReservas =
+                given()
+                .when()
+                .get("https://treinamento-api.herokuapp.com/booking")
+        ;
+
+        responseListagemIdsReservas
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0))
+        ;
+
+
+    }
+}
